c63f74b9089222d26caa9b5d7a592e8abd34980d
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2024-01-03 23:49:42 -0300

    handle with messages and validations

---
+++ internal/services/process/service.go
@@ -0,0 +1,59 @@
+package process
+
+import (
+	messageDao "api-handle/internal/dao/message"
+	"api-handle/internal/model"
+	"api-handle/internal/services/cache"
+	"fmt"
+	"log"
+)
+
+func Init() {
+
+}
+
+func GetAll() []interface{} {
+	return messageDao.GetAllMessages()
+}
+
+func ProccessMessage(message model.Message) error {
+	if messageAlreadyProcessed(message.IdempotenciaKey) {
+		return nil
+	}
+
+	go saveOnCache(message.IdempotenciaKey, model.IN_PROCESS, "Message in process", 600) // 10 minutes cache duration
+
+	err := messageDao.SaveMessage(message)
+
+	if err != nil {
+		log.Printf("Error on save message, [ID]= %s  [error]=%s", message.IdempotenciaKey, err.Error())
+
+		go saveOnCache(message.IdempotenciaKey, model.ERROR_ON_PROCESS, "Error on save message", 600) // 10 minutes cache duration
+
+	}
+
+	go saveOnCache(message.IdempotenciaKey, model.PROCESSED, "Prcessed with success", 60)
+
+	return err
+}
+
+func messageAlreadyProcessed(key string) bool {
+
+	result, err := messageDao.AlreadyProcessed(key)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return result
+
+}
+
+func saveOnCache(key string, status model.Status, message string, duration int) {
+	cacheMessage := model.CacheMessage{
+		ProcessStatus: status,
+		Message:       message,
+	}
+
+	go cache.Set(key, cacheMessage.ToJson(), duration)
+}
